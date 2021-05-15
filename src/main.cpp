097809a02c1685e4061f1d495984f5e15c28e72f
Nathan <kuhl2583@stthomas.edu>
2021-05-14 21:57:49 -0500

    Comments (only scroll bar left)

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <sys/wait.h>
 #include "entries.h"
 
+// Definitions for the width and height of the window
 #define WIDTH 800   
 #define HEIGHT 600
 
@@ -30,11 +31,17 @@ typedef struct AppData {
 
 // Vector of FileEntries containing the various objects whose data will be rendered
 std::vector<FileEntry*> ExplorerEntries;
+// Vector of strings containing just the names of file explorer entries in a specific directory
 std::vector<std::string> RecursiveEntries;
 
+// The user's name
 std::string user;
+// The user's home directory
 std::string home;
 
+/*************************/
+/** FUNCTION PROTOTYPES **/
+/*************************/
 void initialize(SDL_Renderer *renderer, AppData *data_ptr);
 void render(SDL_Renderer *renderer, AppData *data_ptr);
 void renderRecursiveView(SDL_Renderer* renderer, AppData* data_ptr, std::string dirname);
@@ -43,54 +50,74 @@ std::string getDirectoryEntries(std::string dirname, SDL_Renderer* renderer, TTF
 std::string parseMouseClick(int mouse_click_x, int mouse_click_y);
 std::string getFilePermissions(struct stat info);
 
+/*************************/
+/***** MAIN FUNCTION *****/
+/*************************/
 int main(int argc, char **argv)
 {
-    // initializing SDL as Video
+    // Initializing SDL as Video
     SDL_Init(SDL_INIT_VIDEO);
-    // initialize PNG IMG library
+    // Initialize PNG IMG library
     IMG_Init(IMG_INIT_PNG);
+    // Initialize TTF library
     TTF_Init();
 
-    // create window and renderer
-    SDL_Renderer *renderer;    // ONLY NEED TO DO THE FOLLOWING ONE TIME 
-    // load the folder icon
-
+    // Declare a new renderer
+    SDL_Renderer *renderer;  
+    // Declare a new window
     SDL_Window *window;
+    // Create the window and renderer
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
-    // initialize and perform rendering loop
+    // Declare a new AppData
     AppData data;
+    // Call the initialize function to store data for starting the application
     initialize(renderer, &data);
 
+    // Stores the directory currently being viewed
     std::string current_dir;
+    // Call getDirectoryEntries for the user's home directory for startup
     current_dir = getDirectoryEntries(home, renderer, data.font);
 
+    // Start the rendering loop by calling render()
     render(renderer, &data);
     SDL_Event event;
     SDL_WaitEvent(&event);
 
+    // Tracks whether recursive viewing mode is enabled or not
     bool recursive_flag = false;
+    // While the user hasn't quit the program, continue rendering and interpreting events
     while (event.type != SDL_QUIT) 
     {   
+        // If there was a left click by the user, analyze it by looking at its coordinates 
         if(event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
             int mouse_click_x = event.button.x;
             int mouse_click_y = event.button.y;
-
+            
+            // Contains a string representation of the UI element clicked on by the user
             std::string next_element = parseMouseClick(mouse_click_x, mouse_click_y);
+            // Indicated by a comma, a next element type substring is embedded in the next_element string
             std::string next_element_type = next_element.substr(next_element.find(',') + 1, std::string::npos);
+
+            // Once next element type has been extracted, delete the substring from next_element
             int delimiter_loc = next_element.find(',');
             if(delimiter_loc != -1) { next_element.erase(delimiter_loc, std::string::npos); }
 
+            // Parse the various types of next_element
             if(next_element == "NULL") {
-                // do nothing for this click
+                // If NULL, do nothing for this click
             } else if(next_element == "R") {
-                // render recursive viewing mode
+                // If "R", toggle recursive viewing mode either on or off depending on recursive_flag's value 
                 recursive_flag = !recursive_flag;
+                // If the flag is being enabled, build the recursive entries vector in the current directory
                 if(recursive_flag) { buildRecursiveEntries(current_dir, 0); }
             } else if(next_element_type == "" || next_element_type == "dir" || next_element_type == "HOME" || next_element_type == "DESKTOP") {
+                // Clear any previous ExplorerEntries
                 ExplorerEntries.clear();
+                // Save the current directory from the returned value from getDirectoryEntries
                 current_dir = getDirectoryEntries(next_element, renderer, data.font);
             } else {
+                // Otherwise, the selection was made on a file, so open it using fork() and xdg-open
                 int pid = fork();
                 std::string open_file_cmd = "xdg-open " + next_element;
 
@@ -107,10 +134,11 @@ int main(int argc, char **argv)
                 }
             }
         }
+        // Wait for another event
         SDL_WaitEvent(&event);
+        // Recursive flag indicates whether to render the normal file explorer view or recursive view
         if(recursive_flag) {
-            renderRecursiveView(renderer, &data, current_dir); 
-            
+            renderRecursiveView(renderer, &data, current_dir);      
         } else {
             RecursiveEntries.clear();
             render(renderer, &data); 
@@ -118,7 +146,7 @@ int main(int argc, char **argv)
         
     }
 
-    // clean up
+    // Clean up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -127,7 +155,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-// Initializes the rendering color and gathers AppData
+/* Initializes the rendering color and gathers AppData */
 void initialize(SDL_Renderer* renderer, AppData* data_ptr)
 {
     user = getenv("USER");
@@ -138,6 +166,7 @@ void initialize(SDL_Renderer* renderer, AppData* data_ptr)
     data_ptr->font = TTF_OpenFont("resrc/OpenSans-Regular.ttf", 20);
     data_ptr->recursive_font = TTF_OpenFont("resrc/OpenSans-Regular.ttf", 15);
 
+    // Create and store constant icon textures in AppData
     SDL_Surface *surf = IMG_Load("resrc/images/home_icon.png");
     data_ptr->home = SDL_CreateTextureFromSurface(renderer, surf);
     surf = IMG_Load("resrc/images/desktop_icon.png");
@@ -145,36 +174,37 @@ void initialize(SDL_Renderer* renderer, AppData* data_ptr)
     surf = IMG_Load("resrc/images/recursive_icon.png");
     data_ptr->recursiveview = SDL_CreateTextureFromSurface(renderer, surf);
     
+    // Create and store header textures in AppData
     SDL_Color header_color = {0, 0, 0}; 
     surf = TTF_RenderText_Solid(data_ptr->font, "NAME", header_color);
     data_ptr->filename_header = SDL_CreateTextureFromSurface(renderer, surf);
-  
     surf = TTF_RenderText_Solid(data_ptr->font, "SIZE", header_color);
     data_ptr->size_header = SDL_CreateTextureFromSurface(renderer, surf);
-
     surf = TTF_RenderText_Solid(data_ptr->font, "PERMISSIONS", header_color);
     data_ptr->permissions_header = SDL_CreateTextureFromSurface(renderer, surf);
 
     SDL_FreeSurface(surf);
 }
 
-// Uses AppData to render objects and phrases to the window
+/* Uses AppData to render objects and phrases to the window */
 void render(SDL_Renderer* renderer, AppData* data_ptr)
 {
-    // reset render color to gray
+    // Reset render color to gray
     SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
-    // erase renderer content from the previous rendering
+    // Erase renderer content from the previous rendering
     SDL_RenderClear(renderer);
 
-    //x = 85 
+    // Render the constant name header
     SDL_Rect name_header = {135, 7};
     SDL_QueryTexture(data_ptr->filename_header, NULL, NULL, &(name_header.w), &(name_header.h));
     SDL_RenderCopy(renderer, data_ptr->filename_header, NULL, &name_header);
 
+    // Render the constant size header
     SDL_Rect size_header = {475, 7};
     SDL_QueryTexture(data_ptr->size_header, NULL, NULL, &(size_header.w), &(size_header.h));
     SDL_RenderCopy(renderer, data_ptr->size_header, NULL, &size_header);
 
+    // Render the constant permissions header
     SDL_Rect permissions_header = {620, 7};
     SDL_QueryTexture(data_ptr->permissions_header, NULL, NULL, &(permissions_header.w), &(permissions_header.h));
     SDL_RenderCopy(renderer, data_ptr->permissions_header, NULL, &permissions_header);
@@ -189,26 +219,26 @@ void render(SDL_Renderer* renderer, AppData* data_ptr)
     SDL_SetRenderDrawColor(renderer, 152, 153, 155, 255);
     SDL_RenderFillRect(renderer, &scroll_bar);
 
+    // Draw a sidebar for separating buttons from file explorer items
     SDL_Rect sidebar1 = {60, 0, 5, 600};
     SDL_SetRenderDrawColor(renderer, 81, 12, 118, 255);
     SDL_RenderFillRect(renderer, &sidebar1);
 
+    // Render the home, desktop and recursive view icons
     SDL_Rect home = {8, 7, 40, 40};
     SDL_RenderCopy(renderer, data_ptr->home, NULL, &home);
-
     SDL_Rect desktop = {8, 67, 40, 40}; 
     SDL_RenderCopy(renderer, data_ptr->desktop, NULL, &desktop);
-
     SDL_Rect recursiveview = {8, 127, 40, 40};
     SDL_RenderCopy(renderer, data_ptr->recursiveview, NULL, &recursiveview);
 
-    // Create a container for icons: {x, y, width, height}
+    // Create containers where the file explorer items will be rendered into
     SDL_Rect icon_container = {75, 67, 35, 35};
-    
     SDL_Rect name_container = {135, 67};
     SDL_Rect size_container = {475, 67};
     SDL_Rect perms_container = {620, 67};
 
+    // Render each element of the file explorer items stored in ExplorerEntries, increasing the y coordinate by 45 in between renders
     for(int i = 0; i < ExplorerEntries.size(); i++) {
         ExplorerEntries[i]->setCoordinates(icon_container.x, icon_container.x + icon_container.w, 
                                            icon_container.y, icon_container.y + icon_container.h, "icon");
@@ -234,34 +264,40 @@ void render(SDL_Renderer* renderer, AppData* data_ptr)
         perms_container.y += 45;
     }
 
-    // show rendered frame
+    // Show rendered frame
     SDL_RenderPresent(renderer);
 }
 
+/* Render the recursive viewing mode */
 void renderRecursiveView(SDL_Renderer* renderer, AppData* data_ptr, std::string dirname) {
-    // reset render color to gray
+    // Reset render color to gray
     SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
-    // erase renderer content from the previous rendering
+    // Erase renderer content from the previous rendering
     SDL_RenderClear(renderer);
 
+    // Render the sidebar
     SDL_Rect sidebar1 = {60, 0, 5, 600};
     SDL_SetRenderDrawColor(renderer, 81, 12, 118, 255);
     SDL_RenderFillRect(renderer, &sidebar1);
 
+    // Render the recursiveView button
     SDL_Rect recursiveview = {8, 127, 40, 40};
     SDL_RenderCopy(renderer, data_ptr->recursiveview, NULL, &recursiveview);
 
+    // Create a RecursiveTextures array to hold the textures of the RecursiveEntries
     std::vector<SDL_Texture*> RecursiveTextures;
     
     // Transform the string vector into renderable textures, store them in RecursiveTextures
     SDL_Color name_color = {0, 0, 0}; 
     SDL_Surface *surf;
 
+    // Fill the RecursiveTextures array
     for(int i = 0; i < RecursiveEntries.size(); i++) {
         surf = TTF_RenderText_Solid(data_ptr->recursive_font, RecursiveEntries[i].c_str(), name_color);
         RecursiveTextures.push_back(SDL_CreateTextureFromSurface(renderer, surf));
     }
 
+    // Render the directory name
     surf = TTF_RenderText_Solid(data_ptr->recursive_font, dirname.c_str(), name_color);
     SDL_Texture* dir_texture = SDL_CreateTextureFromSurface(renderer, surf);
     SDL_FreeSurface(surf);
@@ -271,6 +307,7 @@ void renderRecursiveView(SDL_Renderer* renderer, AppData* data_ptr, std::string 
     starting_row.x += 25;
     starting_row.y += 25;
 
+    // Render everything inside the directory
     for(int i = 0; i < RecursiveTextures.size(); i++) {
         SDL_QueryTexture(RecursiveTextures[i], NULL, NULL, &(starting_row.w), &(starting_row.h));
         SDL_RenderCopy(renderer, RecursiveTextures[i], NULL, &starting_row);
@@ -279,6 +316,7 @@ void renderRecursiveView(SDL_Renderer* renderer, AppData* data_ptr, std::string 
     SDL_RenderPresent(renderer);
 }
 
+/* Gets everything (files and directories) inside directory 'dirname' and creates FileEntry instances containing information about them */
 std::string getDirectoryEntries(std::string dirname, SDL_Renderer* renderer, TTF_Font* font)
 {
     // Struct containing the information about the current directory
@@ -395,13 +433,21 @@ std::string getDirectoryEntries(std::string dirname, SDL_Renderer* renderer, TTF
     return dirname;
 }
 
+/* Modified version of listDirectory() from class that builds the RecursiveEntries string vector */
 void buildRecursiveEntries(std::string dirname, int indent) {
 
+    // Append spaces depending on the value of 'indent'
     std::string space = "          ";
 	for(int i = 0; i < indent; i++) { space += "          "; }
+
+    // Get info about the current file
     struct stat info;
     int err = stat(dirname.c_str(), &info);
+
+    // Files array of strings stores entry names
     std::vector<std::string> files;
+
+    // Get all the entries in the directory (just their names) and add them to files[]
     if (err == 0 && S_ISDIR(info.st_mode)) {
         DIR* dir = opendir(dirname.c_str());		
 		
@@ -412,46 +458,49 @@ void buildRecursiveEntries(std::string dirname, int indent) {
         closedir(dir);
     }
     
+    // Sort the files array
 	std::sort(files.begin(), files.end());
 	
 	int i, file_err;
 	struct stat file_info;
 	
+    // Recursively add to the RecursiveEntries array
 	for(int i = 0; i < files.size(); i++) {
 		std::string full_path = dirname + "/" + files[i];
 		file_err = stat(full_path.c_str(), &file_info);
 		if(file_err) {
+            // Print an error message (should never happen)
 			fprintf(stderr, "Uh, oh, shouldnt be here");
 		} else if(S_ISDIR(file_info.st_mode)) {
-			printf("%s%s (directory)\n", space.c_str(), files[i].c_str());
+            // Add a directory, and recurse
             RecursiveEntries.push_back(space + files[i]);
 			if(files[i] != "." && files[i] != "..") {
 				buildRecursiveEntries(full_path, indent + 1);
 			}				
 		} else {
+            // Add a file
             RecursiveEntries.push_back(space + files[i]);
-			printf("%s%s \n", space.c_str(), files[i].c_str());
 		}
     }
 }
 
+/* Determine what UI element the user clicked on using x and y coordinates and return a string containing information representing that element */
 std::string parseMouseClick(int mouse_click_x, int mouse_click_y) {
 
     /**** CLICKED ON HOME ICON ****/
     if((mouse_click_x >= 8 && mouse_click_x <= 48) && (mouse_click_y >= 7 && mouse_click_y <= 47)) {
-        // return to the home directory
+        // Return to the home directory
         return home + ",HOME";
     }
-    /**** CLICKED ON ***/
+    /**** CLICKED ON DESKTOP ***/
     else if((mouse_click_x >= 8 && mouse_click_x <= 48) && (mouse_click_y >= 67 && mouse_click_y <= 107))  {
         return home + "/Desktop/,DESKTOP";
-
     }
     /**** CLICKED ON RECURSIVE VIEW ****/
     else if((mouse_click_x >= 8 && mouse_click_x <= 48) && (mouse_click_y >= 127 && mouse_click_y <= 167))  {
         return "R";
-        // render the current directory in recursive view
-        // set recursive mode flag
+
+    /**** CLICKED ON A FILE OR DIRECTORY ****/
     } else {
         for(int i = 0; i < ExplorerEntries.size(); i++) {
             if((mouse_click_x >= ExplorerEntries[i]->icon_coordinates[0] && mouse_click_x <= ExplorerEntries[i]->icon_coordinates[1]) &&
@@ -465,9 +514,11 @@ std::string parseMouseClick(int mouse_click_x, int mouse_click_y) {
             }
         }
     }
+    // Nothing important was clicked on
     return "NULL";
 }
 
+/* Get a string representation of the permissions for a file using its stat structure */
 std::string getFilePermissions(struct stat info) {
     std::string permissions = "";
     if(info.st_mode & S_IRUSR) { permissions.append("r"); } else permissions.append("-");
